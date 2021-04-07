89d528361ec53cb07eba22f8d66396b71d795615
石渡拓海 <t-ishiwata@sight-visit.com>
2021-04-08 00:18:04 +0900

    [Add] mutationの追加

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :goal_create, mutation: Mutations::GoalCreate
+    field :label_create, mutation: Mutations::LabelCreate
   end
 end
