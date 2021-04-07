05fda658cd89fa0aa85893a73464c7692f4260e2
石渡拓海 <t-ishiwata@sight-visit.com>
2021-04-08 00:17:27 +0900

    [Change] titleをnullを許容しない

--- app/graphql/types/goal_type.rb
+++ app/graphql/types/goal_type.rb
@@ -1,7 +1,7 @@
 module Types
   class GoalType < Types::BaseObject
     field :id, ID, null: false
-    field :title, String, null: true
+    field :title, String, null: false
     field :description, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
