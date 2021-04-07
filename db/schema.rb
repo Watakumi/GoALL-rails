b47568a8202c2f941ae2f428e18a17f367cadf2c
石渡拓海 <t-ishiwata@sight-visit.com>
2021-04-07 22:36:46 +0900

    bundle exec rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_06_171441) do
+ActiveRecord::Schema.define(version: 2021_04_07_130844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "goals", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "labels", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
