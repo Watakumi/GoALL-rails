73e9f7feb10e6dfbc9885afd790ee0dee38b28e3
石渡拓海 <t-ishiwata@sight-visit.com>
2021-04-12 01:16:51 +0900

    中間テーブルのアソシエーション の追加

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,2 +1,4 @@
 class Goal < ApplicationRecord
+  has_many :labels, through: :label_goals
+  has_many :label_goals
 end
--- app/models/label.rb
+++ app/models/label.rb
@@ -1,2 +1,4 @@
 class Label < ApplicationRecord
+  has_many: goals, through: :label_goals
+  has_many: label_goals
 end
